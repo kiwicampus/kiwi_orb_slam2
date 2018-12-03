0ddb4b12871bc7e60834ee17f419fee7855fb03a
charlielito <charlie@kiwicampus.com>
2018-12-03 16:13:33 -0500

    feat: use memmap for reading images

--- orb_slam2_ros/src/ros_stereo.cc
+++ orb_slam2_ros/src/ros_stereo.cc
@@ -89,70 +89,40 @@ int main(int argc, char **argv)
     ImageGrabber igb(&SLAM);
 
 
-
-    // MultiImagesMemmap video_map("main_stream", MEMMAP_PATH);
-    // video_map.wait_until_available();
+    MultiImagesMemmap video_map("main_stream", MEMMAP_PATH);
+    video_map.wait_until_available();
 
     camera_info_manager::CameraInfoManager cinfo_manager(nh);
     get_rectify_params_calibration(map1, map2, cinfo_manager);
 
+    ros::NodeHandle n;
+    ros::Rate r(30.0);
 
-
-    stringstream ss(argv[3]);
-	ss >> boolalpha >> igb.do_rectify;
-    igb.do_rectify = false;
-    if(igb.do_rectify)
-    {      
-        // Load settings related to stereo calibration
-        cv::FileStorage fsSettings(argv[2], cv::FileStorage::READ);
-        
-        if(!fsSettings.isOpened())
-        {
-            cerr << "ERROR: Wrong path to settings" << endl;
-            return -1;
-        }
-
-        cv::Mat K_l, K_r, P_l, P_r, R_l, R_r, D_l, D_r;
-        fsSettings["LEFT.K"] >> K_l;
-        fsSettings["RIGHT.K"] >> K_r;
-
-        fsSettings["LEFT.P"] >> P_l;
-        fsSettings["RIGHT.P"] >> P_r;
-
-        fsSettings["LEFT.R"] >> R_l;
-        fsSettings["RIGHT.R"] >> R_r;
-
-        fsSettings["LEFT.D"] >> D_l;
-        fsSettings["RIGHT.D"] >> D_r;
-
-        int rows_l = fsSettings["LEFT.height"];
-        int cols_l = fsSettings["LEFT.width"];
-        int rows_r = fsSettings["RIGHT.height"];
-        int cols_r = fsSettings["RIGHT.width"];
-
-        if(K_l.empty() || K_r.empty() || P_l.empty() || P_r.empty() || R_l.empty() || R_r.empty() || D_l.empty() || D_r.empty() ||
-                rows_l==0 || rows_r==0 || cols_l==0 || cols_r==0)
-        {
-            cerr << "ERROR: Calibration parameters to rectify stereo are missing!" << endl;
-            return -1;
-        }
-
-        cv::initUndistortRectifyMap(K_l,D_l,R_l,P_l.rowRange(0,3).colRange(0,3),cv::Size(cols_l,rows_l),CV_32F,igb.M1l,igb.M2l);
-        cv::initUndistortRectifyMap(K_r,D_r,R_r,P_r.rowRange(0,3).colRange(0,3),cv::Size(cols_r,rows_r),CV_32F,igb.M1r,igb.M2r);
-        ROS_INFO("rectifying image");
-    }
-    ROS_INFO("not rectifying image");
-
-    ros::NodeHandle nodeHandler;
-
-    message_filters::Subscriber<sensor_msgs::Image> left_sub(nodeHandler, "/camera/left/image_raw", 1);
-    message_filters::Subscriber<sensor_msgs::Image> right_sub(nodeHandler, "/camera/right/image_raw", 1);
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_pol;
-    message_filters::Synchronizer<sync_pol> sync(sync_pol(10), left_sub,right_sub);
-    sync.registerCallback(boost::bind(&ImageGrabber::GrabStereo,&igb,_1,_2));
+    // message_filters::Subscriber<sensor_msgs::Image> left_sub(nodeHandler, "/camera/left/image_raw", 1);
+    // message_filters::Subscriber<sensor_msgs::Image> right_sub(nodeHandler, "/camera/right/image_raw", 1);
+    // typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_pol;
+    // message_filters::Synchronizer<sync_pol> sync(sync_pol(10), left_sub,right_sub);
+    // sync.registerCallback(boost::bind(&ImageGrabber::GrabStereo,&igb,_1,_2));
 
     SLAM.Start();
-    ros::spin();
+
+    while (n.ok())
+    {
+        
+        l_image = video_map.read("L");
+        r_image = video_map.read("R");
+
+        if (cinfo_manager.isCalibrated()) {
+        remap(l_image, l_image_undist, map1, map2, INTER_LINEAR);
+        remap(r_image, r_image_undist, map1, map2, INTER_LINEAR);
+
+        igb.mpSLAM->TrackStereo(l_image_undist,r_image_undist, ros::Time::now().toSec());
+
+        }
+        
+        ros::spinOnce(); // check for incoming messages
+        r.sleep();
+    }
 
     // Stop all threads
     SLAM.Shutdown();
